50e8fb2773bd41832cccddf2f6018bf11047b4d1
2nd-deck <archness@naver.com>
2020-09-09 20:59:46 +0900

    Updates

---
+++ precache-manifest.919b0a971e3c0e5d5ea11e151c1fad9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8997319dde239c8d704a6da52ad5d00d",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "f91b9226c1c2a8f159c3",
+    "url": "/movie_app/static/css/main.df2168a9.chunk.css"
+  },
+  {
+    "revision": "821ffe83e7b7fcc36aaa",
+    "url": "/movie_app/static/js/2.a28c4e65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.a28c4e65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f91b9226c1c2a8f159c3",
+    "url": "/movie_app/static/js/main.ec43d70a.chunk.js"
+  },
+  {
+    "revision": "2dd95fdede6216b07a99",
+    "url": "/movie_app/static/js/runtime-main.586cacf9.js"
+  }
+]);
